0100cbef2f8fdcafe121949800072002903505f5
Ali <ali.ozgur@example.com>
2024-07-11 10:14:51 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import IndexScreen from "./screens/IndexScreen";
+import CreateScreen from "./screens/CreateScreen";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      {/*Tüm ekranlarda "blog uygulamsı gözükmesi için  screenOptions={{headerTitle:"Blog Uygulaması"}} yapısı kullanılır.*/}
+      <Stack.Navigator screenOptions={{ headerTitle: "Blog Uygulaması" }}>
+        <Stack.Screen name="Index" component={IndexScreen} />
+        <Stack.Screen name="Create" component={CreateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
